72bb673dc79c245afd58589d213f13f3a6010755
Eskender <eskenderbesrat@gmail.com>
2017-10-24 17:24:48 +0300

    Update topic

--- behavior/scm/topic1.scm
+++ behavior/scm/topic1.scm
@@ -6,6 +6,6 @@
 
 #goal: (please_user=0.4) s: (good robot) ^update_emotion(self_happy .5) thank you
 
-#goal: (please_user=0.4) s: (bad robot) ^update_emotion(self_sad .5) I am sorry
+#goal: (please_user=0.4) s: (bad robot) ^update_emotion(self_happy -.5) I am sorry
 
-#goal: (please_user=0.4) u: (^emotionn(self_happy .8) are you happy) I am happy
+#goal: (please_user=0.4) ?: ( ^emotion(self_happy .8) are you happy) I am happy
