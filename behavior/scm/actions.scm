9b5b3b37235c544975c7705ca3d169dab5a0910a
Eskender <eskenderbesrat@gmail.com>
2017-10-27 12:36:34 +0300

    Add basic topics and some ghost rules for each

---
+++ behavior/scm/actions.scm
@@ -0,0 +1,23 @@
+
+(define-public (true)
+	(stv 1 1)
+)
+
+(define-public (emotion emoWord thresh)
+
+(stv 0 1)
+
+)
+
+
+(define-public (battery thresh)
+
+(stv 0 1)
+
+)
+
+(define-public (hardware_error)
+
+(stv 0 1)
+
+)
